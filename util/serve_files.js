907b2f7c02b49647806afba3d5bdaa5e05820eac
Dmitry Maevsky <dmitry.maevsky@gmail.com>
2020-02-24 23:27:30 +0900

    Support PUT

--- util/serve_files.js
+++ util/serve_files.js
@@ -2,9 +2,9 @@ const path = require('path');
 const fs = require('fs');
 const mime = require('mime/lite');
 
-const { stat, readdir } = fs.promises;
+const { stat, readdir, mkdir, writeFile } = fs.promises;
 
-function send(req, res, file, stats, headers={}) {
+async function sendFile(req, res, file, stats, headers={}) {
   let code=200, opts={};
 
   if (req.headers.range) {
@@ -32,34 +32,51 @@ function send(req, res, file, stats, headers={}) {
   fs.createReadStream(file, opts).pipe(res);
 }
 
+async function sendDirectory(res, filePath) {
+  const items = await readdir(filePath, { withFileTypes: true });
+
+  res.json(items
+    .filter(item => !item.name.startsWith('.') && (item.isDirectory() || item.isFile()))
+    .map(item => ({
+      name: item.name,
+      type: item.isFile() ? 'file' : 'dir'
+    })));
+}
+
 const serve = root => async (req, res) => {
   const filePath = path.join(root, req.path);
   console.log(req.method + ' ' + filePath);
 
-  if (req.method !== 'GET' || !filePath.startsWith(root)) {
+  if (!filePath.startsWith(root)) {
     return res.error('Unauthorized', 401);
   }
 
-  // TODO: cache everything and invalidate in fs watch
-
   try {
     const stats = await stat(filePath);
-    if (stats.isDirectory()) {
-      const items = await readdir(filePath, { withFileTypes: true });
 
-      res.json(items
-        .filter(item => !item.name.startsWith('.') && (item.isDirectory() || item.isFile()))
-        .map(item => ({
-          name: item.name,
-          type: item.isFile() ? 'file' : 'dir'
-        })));
+    if (req.method === 'GET') {
+      // TODO: cache everything and invalidate in fs watch
+      if (stats.isDirectory()) await sendDirectory(res, filePath);
+      else if (stats.isFile()) await sendFile(req, res, filePath, stats);
+      else return res.error('Unsupported resource type', 400);
     }
-    else {
-      send(req, res, filePath, stats);
+    else if (req.method === 'PUT') {
+      if (!stats.isDirectory()) return res.error('Not a directory', 400);
+
+      const { name, type, contents } = req.body;
+      const file = path.join(filePath, name);
+
+      // TODO: set proper access rights mode
+      if (type === 'directory') await mkdir(file);
+      else if (type === 'file') await writeFile(file, contents);
+      else return res.error('Unsupported resource type', 400);
+
+      res.end('success');
     }
+    else res.error('Unsupported method', 400);
   }
   catch (e) {
-    res.error(e.code, 404);
+    res.error(e.code, 400);
   }
 }
 
